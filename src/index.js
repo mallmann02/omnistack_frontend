b3774fe85cbd5086d214ba0ba048e9a416c7adda
leonardo sm <leomallmann020302@gmail.com>
2020-03-24 14:28:20 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'; //JAVASCRIPT + HTML
+import ReactDOM from 'react-dom'; //comunicação da arvore dom com o react
+import App from './App'; //função que retorna um HMTL dentro do javaScrpit
+
+ReactDOM.render(<App />, document.getElementById('root'));
